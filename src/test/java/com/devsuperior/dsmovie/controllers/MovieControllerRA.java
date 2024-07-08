4d4132314a9b7469c248d277d12b6deb35671020
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-08 18:48:14 -0300

    findAllShouldReturnOkWhenMovieNoArgumentsGiven

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,13 +1,13 @@
 package com.devsuperior.dsmovie.controllers;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.is;
 
 
 public class MovieControllerRA {
@@ -15,16 +15,29 @@ public class MovieControllerRA {
 	@BeforeEach
 	public void setUp() {
 		
+		baseURI = "http://localhost:8080";
+		
 	}
 	
 	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
 		
+		given()
+		.get("/movies")
+		.then()
+		.statusCode(200)
+		.body("content.id[0]", is(1))
+		.body("content.title[0]", equalTo("The Witcher"))
+		.body("content.score[0]", is(4.5F))
+		.body("content.count[0]", is(2))
+		.body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"));
+		
 	}
 	
 	@Test
 	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+		
 	}
 	
 	@Test
