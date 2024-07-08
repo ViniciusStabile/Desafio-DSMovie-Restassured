86b68a9bb2bfe939b2a698d58856360bd313015e
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-08 18:20:59 -0300

    First commit

---
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dsmovie.controllers;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+
+
+public class MovieControllerRA {
+	
+	@BeforeEach
+	public void setUp() {
+		
+	}
+	
+	
+	@Test
+	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	}
+	
+	@Test
+	public void findByIdShouldReturnMovieWhenIdExists() {		
+	}
+	
+	@Test
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+	}
+	
+	@Test
+	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
+	}
+	
+	@Test
+	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+	}
+	
+	@Test
+	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+	}
+}
