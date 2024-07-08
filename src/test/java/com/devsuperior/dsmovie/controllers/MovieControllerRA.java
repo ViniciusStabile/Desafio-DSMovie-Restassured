aac04b9be2f24bebf742e204b92f3caae3e59b90
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-08 19:01:44 -0300

    FindAllShouldReturnPagedMoviesWhenMoviesTitleParamIsNotEmpty

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -12,10 +12,15 @@ import org.junit.jupiter.api.Test;
 
 public class MovieControllerRA {
 	
+	
+	private String movieTitle;
+	
 	@BeforeEach
 	public void setUp() {
 		
 		baseURI = "http://localhost:8080";
+		
+	movieTitle = "Matrix";
 		
 	}
 	
@@ -37,6 +42,19 @@ public class MovieControllerRA {
 	
 	@Test
 	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+		
+		given().get("/movies?title={movieTitle}", movieTitle)
+		.then()
+		.statusCode(200)
+		.body("content.id[0]",is(4))
+		.body("content.title[0]",equalTo("Matrix Resurrections"))
+		.body("content.score[0]",is(0.0F))
+		.body("content.count[0]",is(0))
+		.body("content.image[0]",equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"))
+		
+		
+		;
+		
 		
 	}
 	
